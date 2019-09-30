e825210a2eb55d41f036a1b3f28b425f98a9facc
Darius Elphick <dariuselphick@gmail.com>
2019-09-30 20:53:30 +0100

    Enable default lighting

--- adeiladu/GameViewController.swift
+++ adeiladu/GameViewController.swift
@@ -26,20 +26,6 @@ class GameViewController: UIViewController {
         // place the camera
         cameraNode.position = SCNVector3(x: 0, y: 0, z: 15)
         
-        // create and add a light to the scene
-        let lightNode = SCNNode()
-        lightNode.light = SCNLight()
-        lightNode.light!.type = .omni
-        lightNode.position = SCNVector3(x: 0, y: 10, z: 10)
-        scene.rootNode.addChildNode(lightNode)
-        
-        // create and add an ambient light to the scene
-        let ambientLightNode = SCNNode()
-        ambientLightNode.light = SCNLight()
-        ambientLightNode.light!.type = .ambient
-        ambientLightNode.light!.color = UIColor.darkGray
-        scene.rootNode.addChildNode(ambientLightNode)
-        
         let firstLegoCube = SCNBox(width: 15.8, height: 9.6, length: 15.8, chamferRadius: 0.1)
         let firstLegoNode = SCNNode(geometry: firstLegoCube)
         firstLegoNode.position = SCNVector3(x: 0, y: 0, z: 0)
@@ -53,6 +39,8 @@ class GameViewController: UIViewController {
         
         // allows the user to manipulate the camera
         scnView.allowsCameraControl = true
+        
+        scnView.autoenablesDefaultLighting = true
         
         // configure the view
         scnView.backgroundColor = UIColor.black
